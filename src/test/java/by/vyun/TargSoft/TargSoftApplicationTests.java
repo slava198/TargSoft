596b5a9de914bc85e2c3942ef48eaa9d986ff50c
vyun_slava <slava198@tut.by>
2020-10-17 21:21:42 +0300

    init commit

---
+++ src/test/java/by/vyun/TargSoft/TargSoftApplicationTests.java
@@ -0,0 +1,13 @@
+package by.vyun.TargSoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TargSoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
